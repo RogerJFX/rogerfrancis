eb1c2a787c0a08a6480a0895d4b70ed0a4e0b354
roger <roger@crazy-roger.de>
2024-08-19 16:43:59 +0200

    recreate lost version.js

---
+++ js/version.js
@@ -0,0 +1,9 @@
+(function versionFn(_v) {
+    window.rfAppVersion = _v;
+    const l = location;
+    const v = new URL(l.href).searchParams.get('v');
+    if(_v !== v) {
+        l.href = `${l.protocol}//${l.host}${l.pathname}?v=${_v}`;
+    }
+    window.goto = url => l.href = `${url}?v=${_v}`;
+})('0.0.6');
