3dde4ee03ea7b448b5b30bc52dc8c1d74ee984dd
roger <roger@crazy-roger.de>
2024-08-20 14:55:33 +0200

    breadcrumbs

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,45 @@
-(function menuFn(menuNode) {
+(function menuFn(menuNode, breadcrumbsNode) {
 
     const MENU = [
-        {label: 'Home', uri: '/index'},
-        {label: 'My Application', uri: '/page/application/index'},
-        {label: 'Future', uri: '/page/future/index'},
+        {main: true, label: 'Home', uri: '/'},
+        {main: true, label: 'My Application', uri: '/page/application/'},
+        {main: true, label: 'Future', uri: '/page/future/'},
+        {main: false, label: 'Equipment', uri: '/page/application/equipment/'},
+        {main: false, label: 'Guitars', uri: '/page/application/equipment/guitars/'},
+        {main: false, label: 'Amps', uri: '/page/application/equipment/amps/'},
     ];
 
-    (function createMenu() {
+    function findMatchingMenuItems(onlyMain) {
         const pwd = location.pathname;
-        MENU.forEach(i => {
+        return MENU.filter(i => (!onlyMain || i.main) && pwd.startsWith(i.uri)).sort((a, b) => b.uri.length - a.uri.length);
+    }
+
+    (function createMainMenu() {
+        if(!menuNode) return;
+        const pwd = findMatchingMenuItems(true)[0].uri;
+        MENU.filter(i => i.main).forEach(i => {
             const el = document.createElement('SPAN');
             el.classList.add('item');
-            if (pwd.startsWith(i.uri)) {
+            if (i.uri === pwd) {
                 el.classList.add('pwd');
             }
-            el.setAttribute('onclick', `goto('${i.uri}.html')`);
+            el.setAttribute('onclick', `goto('${i.uri}')`);
             el.innerHTML = i.label;
             menuNode.appendChild(el);
         });
     })();
 
-})(document.getElementById('head'));
+    (function createBreadcrumbs() {
+        if(!breadcrumbsNode) return;
+        const matchingItems = findMatchingMenuItems().reverse();
+        matchingItems.forEach(i => {
+            const el = document.createElement('SPAN');
+            el.classList.add('item');
+            el.setAttribute('onclick', `goto('${i.uri}')`);
+            el.innerHTML = i.label;
+            breadcrumbsNode.appendChild(el);
+        })
+        console.log(matchingItems);
+    })();
+
+})(document.getElementById('head'), document.getElementById('breadcrumbs'));
