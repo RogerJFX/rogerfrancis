7101e75329f4dec454d088fad4160e05a9276835
roger <roger@crazy-roger.de>
2024-11-12 13:12:04 +0100

    feat: bundled single track page

--- js/version.js
+++ js/version.js
@@ -1,9 +1,14 @@
-(function versionFn(_v) {
+(function verFn(_v) {
     window.rfAppVersion = _v;
     const l = location;
     const v = new URL(l.href).searchParams.get('v');
     if(_v !== v) {
-        l.href = `${l.protocol}//${l.host}${l.pathname}?v=${_v}`;
+        l.href = cV(l.href);
     }
-    window.goto = url => l.href = `${url}?v=${_v}`;
-})('0.6.2');
+    window.goto = href => l.href = cV(href);
+    function cV(href) {
+        const u = new URL(href, l.href);
+        u.searchParams.set('v', _v);
+        return u.href;
+    }
+})('0.7.0');
