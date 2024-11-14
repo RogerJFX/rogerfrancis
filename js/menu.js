be5140b80eb5acb28482f5911e22195578072013
roger <roger@crazy-roger.de>
2024-11-14 13:18:04 +0100

    feat: evaluate i18 version

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,13 @@
 
     const MENU = [
         {main: true, label: 'Home', uri: '/'},
-        {main: true, label: 'My Application', uri: '/page/application/'},
-        {main: true, label: '🎸 Takes', uri: '/page/takes/'},
-        {main: true, label: 'Thanks', uri: '/page/thanx/'},
-        {main: true, label: 'Future', uri: '/page/future/'},
+        {main: true, label: 'My Application', uri: '/page/application/', i18: {de: 'Bewerbung'}},
+        {main: true, label: '🎸 Takes', uri: '/page/takes/', i18: {de: '🎸 Aufnahmen'}},
+        {main: true, label: 'Thanks', uri: '/page/thanx/', i18: {de: 'Danke'}},
+        {main: true, label: 'Future', uri: '/page/future/', i18: {de: 'Zukunft'}},
+        {main: true, label: '🇬🇧', uri: '/page/lang/', i18: {de: '🇩🇪'}},
         {main: false, label: 'Equipment', uri: '/page/application/equipment/'},
-        {main: false, label: 'Guitars', uri: '/page/application/equipment/guitars/'},
+        {main: false, label: 'Guitars', uri: '/page/application/equipment/guitars/', i18: {de: 'Gitarren'}},
         {main: false, label: 'Amps', uri: '/page/application/equipment/amps/'},
         {main: false, label: 'Pedals', uri: '/page/application/equipment/pedals/'},
         {main: false, label: 'Recording', uri: '/page/application/equipment/recording/'},
@@ -29,7 +30,7 @@
             } else {
                 el.setAttribute('onclick', `goto('${i.uri}')`);
             }
-            el.innerHTML = i.label;
+            el.innerHTML = findLabel(i);
             menuNode.appendChild(el);
         });
     })();
@@ -47,7 +48,7 @@
         }
         const matchingItems = findMatchingMenuItems().reverse();
         matchingItems.forEach(i => {
-            const el = createItem(i.label);
+            const el = createItem(findLabel(i));
             el.setAttribute('onclick', `goto('${i.uri}')`);
             breadcrumbsNode.appendChild(el);
         });
@@ -59,5 +60,32 @@
             document.title = matchingItems.reverse()[0].label;
         }
     })(110, 30);
+
+    window.goto = href => checkI18AvailAndDoGoto(href);
+
+    function findLabel(item) {
+        const lang = sessionStorage.getItem('lang');
+        if(lang && item.i18 && item.i18[lang]) {
+            return item.i18[lang];
+        }
+        return item.label;
+    }
+
+    function checkI18AvailAndDoGoto(href, fn) {
+        const requestedLang = sessionStorage.getItem('lang');
+        if(!requestedLang) {
+            location.href = window.impHref(href);
+        }
+        const xhr = new XMLHttpRequest();
+        xhr.onload = _ => {
+            if(xhr.readyState !== 4 || xhr.status === 200) {
+                location.href = window.impHref(href + `index-${requestedLang}.html`);
+            } else {
+                location.href = window.impHref(href);
+            }
+        }
+        xhr.open('HEAD', href + `index-${requestedLang}.html`);
+        xhr.send(null);
+    }
 
 })(document.getElementById('head'), document.getElementById('breadcrumbs'));
