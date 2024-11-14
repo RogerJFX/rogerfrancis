f8a0d29223d928fccf8e5a1523dcdac6b6f93608
roger <roger@crazy-roger.de>
2024-11-14 16:10:51 +0100

    chore: some new message

--- js/version.js
+++ js/version.js
@@ -4,4 +4,4 @@ const l=location;
 if(_v!==new URL(l.href).searchParams.get('v')){l.href=c(l.href);}
 window.goto=x=>l.href=c(x);
 function c(x){const u=new URL(x,l.href);u.searchParams.set('v',_v);return u.href;}
-})('0.7.4');
+})('0.7.5');
